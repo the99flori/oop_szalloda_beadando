9abf93180c0b5350df8c1a923d2fc223b2560474
Flórián Demecs <demecsflorian@gmail.com>
2024-05-10 21:36:15 +0200

    make child classes - EgyagyasSzoba, KetagyasSzoba

--- app.py
+++ app.py
@@ -14,5 +14,25 @@ class Szoba(ABC):
     def get_description(self):
         pass
 
+class EgyagyasSzoba(Szoba):
+    def __init__(self, szama:int):
+        super().__init__(ar=10000, szama=szama)
+
+    def get_description(self):
+        return f'Egyágyas szoba, szoba száma: {self.szama}, ára: {self.ar} Ft/éj'
+    
+class KetagyasSzoba(Szoba):
+    def __init__(self, szama:int):
+        super().__init__(ar=15000, szama=szama)
+
+    def get_description(self):
+        return f'Kétágyas szoba, szoba száma: {self.szama}, ára: {self.ar} Ft/éj'
+
 if __name__ == '__main__':
+
+    egyagyas_szoba = EgyagyasSzoba(101)
+    print(egyagyas_szoba.get_description())
+    ketagyas_szoba = KetagyasSzoba(201)
+    print(ketagyas_szoba.get_description())
+    
     wait = input('Nyomj entert a folytatáshoz...')
