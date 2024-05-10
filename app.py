02e7e4df489d3ceee0fc2ed32dcf76535222defb
Flórián Demecs <demecsflorian@gmail.com>
2024-05-10 21:26:40 +0200

    add Szoba class

--- app.py
+++ app.py
@@ -2,3 +2,17 @@
 GDE 2023/24/2 - Objektumorientált programozás - beadandó dolgozat
 Szállodai szobafoglaló alkalmazás - készítette: Demecs Flórián - HBJU1F
 '''
+
+from abc import ABC, abstractmethod
+
+class Szoba(ABC):
+    def __init__(self, ar:int, szama:int):
+        self.ar = ar
+        self.szama = szama
+
+    @abstractmethod
+    def get_description(self):
+        pass
+
+if __name__ == '__main__':
+    wait = input('Nyomj entert a folytatáshoz...')
